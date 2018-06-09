ff94eaac0be73c39db3f41f51aeabdf507cb699b
Henrique Grana <granaz256@gmail.com>
2018-06-08 23:44:58 -0300

    preparing files

---
+++ chain.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * Kunai Com Corrente
+ * 
+ * kunai.js goes into the site WITH the sha256 of the folder, then it can load the scripts from the kunais's site folder;
+ */
+
+// Setting the field for the injection;
+var url = location.href;
---
+++ kunais/firstmatter/kunai.js
@@ -0,0 +1,4 @@
+/**
+ * Calling the files from the folder, call as many files as you want;
+ */
+kunaing("main");
